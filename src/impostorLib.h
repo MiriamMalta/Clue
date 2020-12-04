519d0c68bcd52ac9b0c8ba406fea94694e5a8642
Darksw20 <ricardog.navi20@gmail.com>
2020-12-04 01:40:34 -0600

    SOlved many things

--- src/impostorLib.h
+++ src/impostorLib.h
@@ -27,6 +27,20 @@ typedef enum {
 } GameScreen;
 
 typedef enum {
+    sLOGO, 
+    sMENU,
+    sBLACK,
+    sRAYLOGO,
+    sCREDITS,
+    sSAVE,
+    sLOAD,
+    sNEWGAME,
+    sPAUSE,
+    sSETTINGS,
+    
+} ScreenRes;
+
+typedef enum {
     PLACES,
     CHARACTER,
     DEATHS
@@ -88,10 +102,15 @@ struct ImpostorGame{
     int                 fps;
     int                 playersAlive;
     int                 resolution;
+    int                 btnWidth;
+    int                 btnHeight;
+    int                 btnInitialPosition;
+    int                 btnRowDistance;
+    int                 btnCenter;
     float               speed;
     int                 typeTBActive[6];
     bool                typeTBEditMode[6];
-    char*               nameTB[6];
+    char               nameTB[6][30];
     bool                nameEditMode[6];
     char*               colorTB[6];
     Card                envelope;               // Cards inside the envelope
